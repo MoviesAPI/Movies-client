74940894e59b2e0564ef9b9f6f791bdab34ea467
susiantiamelia <ameliasusianti@gmail.com>
2018-07-12 18:35:03 +0700

    first-commit

--- client/src/components/loginForm.vue
+++ client/src/components/loginForm.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="hello">
-
-<h1>masuk</h1>
-
+    <h1>masuk lagi</h1>
   </div>
 </template>
 
@@ -12,7 +10,5 @@ export default {
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 
-<style scoped lang="scss">
-
-</style>
