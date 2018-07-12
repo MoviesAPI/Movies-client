42834e87e765c34300c52fef652f3d95a0659a43
ariefardi <forzaariefpyongmail.com>
2018-07-12 23:07:52 +0700

    add search page

--- client/src/router.js
+++ client/src/router.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import searchResult from './views/searchResult.vue'
 
 Vue.use(Router)
-
+import {mapState, mapActions} from 'vuex'
 export default new Router({
   routes: [
     {
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: searchResult
     }
   ]
 })
--- client/src/store.js
+++ client/src/store.js
@@ -5,12 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    show : false,
+    searchDate : [],
+    searchQuery : ''
   },
   mutations: {
 
   },
   actions: {
-
+    getVideo (context) {
+      console.log('store get video')
+    },
+    getFilmBySearch (context) {
+      console.log('get Film')
+    },
+    searchFilm (context) {
+      console.log('search film')
+      console.log(this.state.searchQuery)
+    }
   }
 })
