708db1a678106d9e87aa9355089c47b0eb660d10
susiantiamelia <ameliasusianti@gmail.com>
2018-07-13 09:38:26 +0700

    third commit

--- client/src/components/SignupForm.vue
+++ client/src/components/SignupForm.vue
@@ -1,30 +1,33 @@
 <template>
     <div id="signupbox" style="display:block; margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="panel-heading">
-            <div class="panel-title">Sign Up</div>
+            <div class="panel-title">Sign Up Here!</div>
         </div>
 
         <div class="panel-body" >
             <form id="signupform" class="form-horizontal" role="form">
   
                 <div class="form-group">
-                <label id="label" for="email" class="col-md-3 control-label">Email</label>
                     <div class="col-md-9 center">
                         <input type="text" class="form-control" name="email" placeholder="Email Address">
                     </div>
                 </div>
                                     
                 <div class="form-group">
-                    <label for="firstname" class="col-md-3 control-label">Full Name</label>
                         <div class="col-md-9">
                             <input type="text" class="form-control" name="firstname" placeholder="First Name">
                          </div>
                         </div>
                 <div class="form-group">
-                    <label for="password" class="col-md-3 control-label">Password</label>
                         <div class="col-md-9">
                             <input type="password" class="form-control" name="passwd" placeholder="Password">
                         </div>
+                </div>
+                
+                <div style="margin-top:10px" class="form-button">
+                    <div class="col-sm-12 controls">
+                        <a id="btn-signup"  class="btn btn-success">Sign Up</a>
+                    </div>
                 </div>
 
                 <div id="or" class="form-button">
@@ -34,8 +37,7 @@
                 </div>
                 <div style="margin-top:10px" class="form-button">
                     <div class="col-sm-12 controls">
-                        <a id="btn-login"  class="btn btn-success">Sign Up  </a>
-                        <a id="btn-fblogin"  class="btn btn-primary">Sign Up with Facebook</a>
+                        <a id="btn-fbsignup"  class="btn btn-primary">Sign Up with Facebook</a>
                     </div>
                 </div>
                 
@@ -57,30 +59,62 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style>
-#btn-fblogin {
+#btn-fbsignup {
   margin-left: 10px;
+  color: white;
   /* border: 1px solid black; */
+}
+
+#btn-signup{
+    color: white;
 }
 
 #signupbox {
-  margin-left: 350px;
-  /* border: 1px solid black; */
+    margin-left: 350px
+}
+
+#signupform{
+    border:1px solid steelblue;
+	width: 400px;
+	margin-left: auto;
+	margin-right: auto;
+	padding:30px 20px 50px 20px;
+	background-color: white;
+	text-align: center;
+	border-radius: 5px;
+}
+
+.panel-heading{
+    margin-left: 128px;
+    border: 1px solid steelblue;
+    width: 61%;
+    border-radius: 5px;
+    padding: 10px;
+    font-size: 20px;
+    background-color: steelblue;
+}
+
+.panel-title{
+    color: white;
 }
 
 .form-control {
-  margin-left: 210px;
+  margin-left: 30px;
   /* border: 1px solid black; */
-    width: 60%;
+    width: 270px;
 
 }
 
 .form-button {
-  margin-right: -80px;
+    padding: 8px;
+    margin-right: -8px;
   /* border: 1px solid black; */
 }
 
 #or {
-  margin-right: -300px;
+    padding: 0px;
+  margin-right: -110px;
+  font-size: 20px;
   /* border: 1px solid black; */
 }
 
